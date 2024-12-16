0a37f4355b26b01b4c39e72df3760fd13a077b19
DenisPepper <thech@yandex.ru>
2024-12-16 10:19:29 +0300

    Добавит стили для активной ссылки

---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink as Link } from 'vue-router';
+</script>
+
+<template>
+  <nav>
+    <ul class="links">
+      <li class="link"><Link active-class="active" to="/">Home page</Link></li>
+      <li class="link"><Link active-class="active" to="/about">About page</Link></li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+.links {
+  height: 3rem;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  background-color: gray;
+}
+
+.link *:any-link {
+  color: black;
+  text-decoration: none;
+}
+
+.link *:any-link.active {
+  color: brown;
+}
+</style>
